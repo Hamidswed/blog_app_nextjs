c287881206be15c860744d535a3b53b1d043da8f
Hamidreza <delshad.swdn@gmail.com>
2024-09-01 18:05:57 +0330

    add react hook form,yup and axios, make auth group

--- frontend/start/src/app/(auth)/signup/page.jsx
+++ frontend/start/src/app/(auth)/signup/page.jsx
@@ -1,21 +1,70 @@
-import TextField from "@/ui/TextField";
+"use client";
 
-export const metadata = {
-  title: "ثبت نام",
-};
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// export const metadata = {
+//   title: "ثبت نام",
+// };
+
+const schema = yup
+  .object({
+    name: yup.string().min(5, "نام و نام خانوادگی نا معتبر است").max(30).required("نام و نام خانوادگی الزامی است"),
+    email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
 
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div>
-      <h1>ثبت نام</h1>
-      <form>
-        <TextField
-          name="name"
+      <h1 className="text-xl font-bold text-secondary-500 text-center mb-6">
+        ثبت نام
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+        <RHFTextField
           label="نام و نام خانوادگی"
-          // value={}
-          // onChange={}
+          name="name"
+          register={register}
+          errors={errors}
           isRequired
         />
+        <RHFTextField
+          label="ایمیل"
+          name="email"
+          register={register}
+          dir="ltr"
+          errors={errors}
+          isRequired
+        />
+        <RHFTextField
+          label="رمز عبور"
+          name="password"
+          register={register}
+          type="password"
+          dir="ltr"
+          errors={errors}
+          isRequired
+        />
+        <Button variant="primary" type="submit" className="w-full">
+          تایید
+        </Button>
       </form>
     </div>
   );
