683817385f835cb0331921337f48baf5678f666c
Hamidreza <delshad.swdn@gmail.com>
2024-11-19 14:11:45 +0330

    add drawer menu

--- frontend/start/src/app/(dashboard)/profile/_components/SideBar.jsx
+++ frontend/start/src/app/(dashboard)/profile/_components/SideBar.jsx
@@ -3,11 +3,13 @@
 import {
   ArrowLeftStartOnRectangleIcon,
   HomeIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import SideBarNavs from "./SideBarNavs";
+import ButtonIcon from "@/ui/ButtonIcon";
 
-function SideBar() {
+function SideBar({ onClose }) {
   // const { logout } = useAuth();
 
   const logoutHandler = async () => {
@@ -16,16 +18,26 @@ function SideBar() {
 
   return (
     <div className="overflow-y-auto flex flex-col p-5 h-screen pt-10 lg:pt-8">
-      {/* Drawer header */}
-      <Link
-        href="/"
-        className="flex items-center gap-x-4 justify-center text-secondary-700 border-b  border-b-secondary-200 
-        pb-2 mb-6"
-      >
-        <HomeIcon className="w-6 h-6" />
-        <span> نکست بلاگ</span>
-      </Link>
-      {/* Drawer content */}
+      {/* Sidbar header */}
+      <div className="flex items-center justify-between w-full mb-5 pb-2 border-b border-b-secondary-200">
+        <Link
+          href="/"
+          className="flex items-center gap-x-4 justify-center text-secondary-700 lg:flex-1"
+        >
+          <HomeIcon className="w-6 h-6" />
+          <span> نکست بلاگ</span>
+        </Link>
+
+        <ButtonIcon
+          className="block lg:hidden border-none"
+          variant="outline"
+          onClick={onClose}
+        >
+          <XMarkIcon />
+        </ButtonIcon>
+      </div>
+
+      {/* Sidebar content */}
       <div className="overflow-y-auto flex-auto">
         <SideBarNavs />
         <div
