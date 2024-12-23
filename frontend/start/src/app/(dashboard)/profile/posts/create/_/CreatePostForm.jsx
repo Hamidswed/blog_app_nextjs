1cd6071502d414a23cfd1ce5376a57be8c7bc53b
Hamidreza <delshad.swdn@gmail.com>
2024-12-23 20:26:39 +0330

    add image cover function in creating post

--- frontend/start/src/app/(dashboard)/profile/posts/create/_/CreatePostForm.jsx
+++ frontend/start/src/app/(dashboard)/profile/posts/create/_/CreatePostForm.jsx
@@ -3,20 +3,29 @@
 import { useCategories } from "@/hooks/useCategories";
 import RHFSelect from "@/ui/RHFSelect";
 import RHFTextField from "@/ui/RHFTextField";
+import TextField from "@/ui/TextField";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import { useState } from "react";
 import * as yup from "yup";
+import Image from "next/image";
+import ButtonIcon from "@/ui/ButtonIcon";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import FileInput from "@/ui/FileInput";
 
 const schema = yup.object();
 
 function CreatePostForm() {
   const { categories } = useCategories();
+  const [coverImageUrl, setCoverImageUrl] = useState(null);
 
   const {
+    control,
     register,
     formState: { errors },
     handleSubmit,
     reset,
+    setValue,
   } = useForm({
     mode: "onTouched",
     resolver: yupResolver(schema),
@@ -66,6 +75,49 @@ function CreatePostForm() {
         register={register}
         options={categories}
       />
+      <Controller
+        name="coverImage"
+        control={control}
+        rules={{ required: "کاور پست الزامی است" }}
+        render={({ field: { value, onChange, ...rest } }) => {
+          return (
+            <FileInput
+              type="file"
+              label="انتخاب کاور پست"
+              name="coverImage"
+              isRequired
+              {...rest}
+              value={value?.fileName}
+              onChange={(event) => {
+                const file = event.target.files[0];
+                onChange(file);
+                setCoverImageUrl(URL.createObjectURL(file));
+                event.target.value = null; //when you chose a same image it doesn't upload. so it's a trick.
+              }}
+            />
+          );
+        }}
+      />
+      {coverImageUrl && (
+        <div className="relative aspect-video overflow-hidden rounded-lg">
+          <Image
+            fill
+            alt="cover-image"
+            src={coverImageUrl}
+            className="object-cover object-center"
+          />
+          <ButtonIcon
+            variant="red"
+            className="w-6 h-6 absolute left-2 top-2"
+            onClick={() => {
+              setCoverImageUrl(null);
+              setValue("coverImage", null); //it must be removed from state of hook form too.
+            }}
+          >
+            <XMarkIcon />
+          </ButtonIcon>
+        </div>
+      )}
     </form>
   );
 }
